e8e7ad99e4447a4fb4823fa7dd5accbf915db943
deevee47 <divyanshvishwakarma4@gmail.com>
2024-12-21 12:41:39 +0530

    feat: responsive for mbile screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,17 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div className="top-6 fixed p-4 flex justify-between bg-black/20 rounded-xl backdrop-blur-lg w-[80%] mx-auto z-50 items-center shadow-lg left-1/2 transform -translate-x-1/2">
-            <div className="text-2xl font-bold text-white">MagicCanvas</div>
+        <div className="top-6 fixed p-4 flex justify-between bg-black/20 rounded-xl backdrop-blur-lg w-[90%] sm:w-[80%] mx-auto z-50 items-center shadow-lg left-1/2 transform -translate-x-1/2">
+            <div className="text-lg sm:text-2xl font-bold text-white">MagicCanvas</div>
 
-            <div className="flex space-x-4">
+            <div className="flex space-x-2 sm:space-x-4">
                 <Link href="https://github.com/deevee47" target="_blank">
-                    <ShinyButton><GithubIcon /></ShinyButton>
+                    <ShinyButton>
+                        <GithubIcon className="h-5 w-5 sm:h-6 sm:w-6" />
+                    </ShinyButton>
                 </Link>
                 <Link href="https://twitter.com/deevee47" target="_blank">
-                    <ShinyButton>Hire Me 😉</ShinyButton>
+                    <ShinyButton className="text-sm sm:text-base">Hire Me 😉</ShinyButton>
                 </Link>
             </div>
         </div>
